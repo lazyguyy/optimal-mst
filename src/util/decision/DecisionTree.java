7b0de67c31317c071e56a44041e7ba5613c15f96
slyphiX <slyphiX@users.noreply.github.com>
2018-02-28 18:09:51 +0100

    first draft at decision trees

--- src/util/decision/DecisionTree.java
+++ src/util/decision/DecisionTree.java
@@ -1,24 +1,40 @@
 package util.decision;
 
+import java.util.Comparator;
+
 public class DecisionTree {
 
     // internally, we represent a decision tree (quite similar to a binary heap) by an array of nodes
     // each describing a comparison between two edges
     private Comparison[] comparisons;
 
-    public DecisionTree(int depth) {
+    private DecisionTree(int depth, Comparison[] comparisons) {
 
     }
 
-    int bucket(/*EdgePermutation permutation*/) {
+    public <E> int bucket(E[] elements, Comparator<? super E> comparator) {
 
-        return 0;
+        int index = 0;
+        while (index < comparisons.length) {
+            Comparison c = comparisons[index];
+            E first = elements[c.firstIndex];
+            E second = elements[c.secondIndex];
+            // if first < second
+            if (comparator.compare(first, second) < 0) {
+                // go to left child
+                index = 2 * index + 1;
+            } else {
+                // go to right child
+                index = 2 * index + 2;
+            }
+        }
+        return index - comparisons.length;
     }
 
 
     // represents a comparison between edges
     private class Comparison {
-        int firstEdge;
-        int secondEdge;
+        int firstIndex;
+        int secondIndex;
     }
 }
