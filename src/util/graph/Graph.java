7fdd1a18771fc5f4c6d720e364cced899a20a76c
slyphiX <slyphiX@users.noreply.github.com>
2018-03-01 22:32:54 +0100

    added missing class

---
+++ src/util/graph/Graph.java
@@ -0,0 +1,18 @@
+package util.graph;
+
+import util.graph.edge.DirectedEdge;
+
+public class Graph<E extends DirectedEdge<E>> {
+    public final int vertices;
+    public final EdgeList<E> edges;
+
+    public Graph(int vertices, EdgeList<E> edges) {
+        this.vertices = vertices;
+        this.edges = edges;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Graph(vertices=%s, edges=\n%s)", vertices, edges);
+    }
+}
