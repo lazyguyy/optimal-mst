54ad2b758a7fe6c0755b988de875fda4d078c7a7
lazyguyy <shelton.kueken@gmail.com>
2018-03-07 21:03:11 +0100

    added graphs and the skript used for generating them

---
+++ data/generate_weighted_graph.py
@@ -0,0 +1,23 @@
+import random
+
+# Takes a number of vertices and a floating point number between 0 and 1
+# And generates a graph that has that many vertices and an edge between
+# and two given vertices with the given probability
+
+vertices = int(input())
+edge_prob = float(input())
+
+graph = []
+edges = 0
+
+for i in range(vertices - 1):
+    for j in range(i + 1 ,vertices):
+        if random.random() < edge_prob:
+            graph.append((i, j))
+            edges += 1
+
+
+weights = list(range(1, edges + 1))
+random.shuffle(weights)
+for i in range(edges):
+    print(graph[i][0], graph[i][1], weights[i])
